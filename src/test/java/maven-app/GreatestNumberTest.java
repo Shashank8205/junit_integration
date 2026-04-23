8de976464084598015b7e9b901ed50bbaddc82c4
Shashank Upadhya J <shasj0825@gmail.com>
2026-04-24 01:15:30 +0530

    Update GreatestNumberTest.java

--- src/test/java/maven-app/GreatestNumberTest.java
+++ src/test/java/maven-app/GreatestNumberTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 public class GreatestNumberTest {
 GreatestNumber g=new GreatestNumber(); 
-    
+    @Test
     public void greatestone(){
         int result=g.greatest(3,5);
 		assertEquals(5,result);
